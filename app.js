70d74483005bf309e449e7363906531339801117
Vaclav Mlejnsky <vasek.mlejnsky@gmail.com>
2020-04-29 14:35:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello from Render!"));
+app.get("/", (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log('IP:', ip);
+  console.log('req.baseUrl:', req.baseUrl);
+  console.log('req.hostname:', req.hostname);
+  console.log('req.ip:', req.ip);
+  console.log('req.originalUrl:', req.originalUrl);
+  res.send('ip = ' + ip);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
